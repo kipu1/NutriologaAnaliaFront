9ab5f508ae4cc236a4fa461a00c2ac99650c7c00
Kevin Ortiz <111302391+kipu1@users.noreply.github.com>
2024-09-07 15:43:38 -0500

    cambios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,12 +12,55 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  nombreUsuario: string | null = '';
+  currentSlide: number = 0;
+  totalSlides: number = 3;
 
   constructor(private router: Router) {}
+
   ngOnInit(): void {}
 
-  /* ngOnInit() {
+  servicios(): void {
+    this.router.navigate(['/servicios']);
+  }
+
+  conoceme(): void {
+    this.router.navigate(['/conoceme']);
+  }
+
+  // Función para ir al slide anterior
+  prevSlide() {
+    this.pauseCurrentVideo(); // Pausar el video actual antes de cambiar de slide
+    this.currentSlide =
+      this.currentSlide === 0 ? this.totalSlides - 1 : this.currentSlide - 1;
+    this.updateSlidePosition();
+  }
+
+  // Función para ir al siguiente slide
+  nextSlide() {
+    this.pauseCurrentVideo(); // Pausar el video actual antes de cambiar de slide
+    this.currentSlide =
+      this.currentSlide === this.totalSlides - 1 ? 0 : this.currentSlide + 1;
+    this.updateSlidePosition();
+  }
+
+  // Actualizar la posición del carrusel
+  updateSlidePosition() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    carousel.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+  }
+
+  // Pausar el video actual cuando cambias de slide
+  pauseCurrentVideo() {
+    const currentVideo = document.querySelectorAll('.carousel-video')[
+      this.currentSlide
+    ] as HTMLVideoElement;
+    if (currentVideo && !currentVideo.paused) {
+      currentVideo.pause();
+    }
+  }
+}
+//nombreUsuario: string | null = '';
+/* ngOnInit() {
     const currentUser = localStorage.getItem('currentUser');
     console.log('Usuario logueado:', currentUser); // Verifica en la consola si el currentUser tiene nombre y token
 
@@ -32,10 +75,3 @@ export class HomeComponent implements OnInit {
       this.nombreUsuario = 'No hay usuario logueado';
     }
   }*/
-  servicios(): void {
-    this.router.navigate(['/servicios']);
-  }
-  conoceme(): void {
-    this.router.navigate(['/conoceme']);
-  }
-}
