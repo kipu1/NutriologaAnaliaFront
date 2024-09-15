b0dfaffd9d0dc146a30e95ee005a263e93b20c2a
Kevin Ortiz <111302391+kipu1@users.noreply.github.com>
2024-09-15 07:46:04 -0500

    c

--- src/app/environment/environment.pro.ts
+++ src/app/environment/environment.pro.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://analiaroman.netlify.app/api', // URL de tu backend desplegado
+  apiUrl: 'https://analiaromannutricionista.netlify.app/api', // URL de tu backend desplegado
 };
